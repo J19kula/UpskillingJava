f2d9853aa91754881903a53744cac57175d7ef5b
J19kula <j19kula@gmail.com>
2022-01-06 15:29:04 -0500

    added a getArea method to polygon to allow for override

--- ProjectDemo_01/src/com/revature/jan6/calculus/main3.java
+++ ProjectDemo_01/src/com/revature/jan6/calculus/main3.java
@@ -4,8 +4,10 @@ public class main3 {
     public static void main(String[] args) {
         triangle t1 = new triangle(5, 8);
         rectangle r1 = new rectangle(10, 12);
+        polygon p1 = new polygon(12.0, 10.0, 2.0);
 
         System.out.println("Area of "+t1 + " is "+t1.getArea());
         System.out.println("Area of "+r1+ " is "+r1.getArea());
+        System.out.println("Area of "+p1+ " is "+p1.getArea());
     }
 }
--- ProjectDemo_01/src/com/revature/jan6/calculus/polygon.java
+++ ProjectDemo_01/src/com/revature/jan6/calculus/polygon.java
@@ -3,6 +3,7 @@ package com.revature.jan6.calculus;
 public class polygon {
     double dim1;
     double dim2;
+    double numSides;
 
     public double getDim1() {
         return dim1;
@@ -25,11 +26,22 @@ public class polygon {
         this.dim2 = dim2;
     }
 
+    public polygon(double dim1, double dim2, double numSides){
+        this.dim1 = dim1;
+        this.dim2 = dim2;
+        this.numSides = numSides;
+    }
+
     public polygon(){
 
     }
 
     public String toString(){
-        return "Polygon [dim1: "+ dim1 + ", dim2: " + dim2 + "]";
+        return "Polygon [dim1: "+ dim1 + ", dim2: " + dim2 + ", numSides: "+numSides+ "]";
+    }
+
+    public double getArea(){
+        double apothem = (dim1)/Math.abs(2*(Math.tan(180/numSides)));
+        return (numSides * dim1*apothem)/2;
     }
 }
