94a2136b8b82cbe44e03f5ae3ba6aaa305058c54
J19kula <j19kula@gmail.com>
2022-01-19 12:32:10 -0500

    Comments

--- ProjectDemo_01/src/com/revature/jan19/SpringDemo_01/App_01.java
+++ ProjectDemo_01/src/com/revature/jan19/SpringDemo_01/App_01.java
@@ -1,24 +1,24 @@
-package com.revature.jan19.SpringDemo_01;
-
-import com.revature.jan19.SpringDemo_01.beans.HelloBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class App_01 {
-    public static void main(String[] args) {
-        ApplicationContext ctxt = new ClassPathXmlApplicationContext("spring.xml");
-        ctxt.getBean("bean");
-
-        HelloBean helloBean = (HelloBean)ctxt.getBean("bean");
-        HelloBean otherBean = (HelloBean)ctxt.getBean("bean");
-
-        System.out.println(helloBean.getSlno() + " " + helloBean.getName());
-
-        if (helloBean == otherBean) {
-            System.out.println("Single bean");
-        }
-        else {
-            System.out.println("Different beans");
-        }
-    }
-}
+//package com.revature.jan19.SpringDemo_01;
+//
+//import com.revature.jan19.SpringDemo_01.beans.HelloBean;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class App_01 {
+//    public static void main(String[] args) {
+//        ApplicationContext ctxt = new ClassPathXmlApplicationContext("spring.xml");
+//        ctxt.getBean("bean");
+//
+//        HelloBean helloBean = (HelloBean)ctxt.getBean("bean");
+//        HelloBean otherBean = (HelloBean)ctxt.getBean("bean");
+//
+//        System.out.println(helloBean.getSlno() + " " + helloBean.getName());
+//
+//        if (helloBean == otherBean) {
+//            System.out.println("Single bean");
+//        }
+//        else {
+//            System.out.println("Different beans");
+//        }
+//    }
+//}
